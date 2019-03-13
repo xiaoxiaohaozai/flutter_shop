0c21f89dfbb4f8caaf252b35a3623abc7aa4c055
ch <ace_haochen@163.com>
2019-03-13 23:00:32 +0800

    Get请求和动态组件协作

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController typeController = TextEditingController();
+  String showText = '欢迎你来到美好人间';
+
   @override
   Widget build(BuildContext context) {
-    getHttp();
-    return Scaffold(
-      body: Center(
-        child: Text('商城'),
+    return Container(
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text('美好人间'),
       ),
-    );
+      body: SingleChildScrollView(
+        child: Container(
+          height: 1000,
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: typeController,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(10),
+                  labelText: '美女类型',
+                  helperText: '请输入你喜欢的类型',
+                ),
+                autofocus: false, //静止聚焦
+              ),
+              RaisedButton(
+                onPressed: _choiceAction,
+                child: Text('选择完毕'),
+              ),
+              Text(
+                showText,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 
-//模拟异步
-  void getHttp() async {
+  void _choiceAction() {
+    print('开始选择你喜欢的类型......');
+    if (typeController.text.toString().isEmpty) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('输入内容为空'),
+              ));
+    } else {
+      //从网络获取内容并展示
+      getHttp(typeController.text.toString()).then((value) {
+        setState(() {
+          showText = value['data']['name'].toString();
+        });
+      });
+    }
+  }
+
+//模拟网络请求
+  Future getHttp(String typeText) async {
     try {
-      Response response = await Dio().get(
-        "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=CH",
-      );
-      print(response);
+      Response response;
+      //构建参数
+      var data = {'name': typeText};
+
+      response = await Dio().get(
+          "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian",
+          queryParameters: data);
+      return response.data;
     } catch (e) {
       print(e);
     }
